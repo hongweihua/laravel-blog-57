331e66e11d249e1af1c720d15db728948261b003
huaziking <751997661@qq.com>
2019-12-23 15:49:12 +0800

    add api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('throttle:60,1')->prefix('v1')->group(function() {
+    Route::get('/articles', 'API\PostController@index');
+});
